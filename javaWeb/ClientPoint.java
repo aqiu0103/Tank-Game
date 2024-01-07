063f285b12911b0127ed60386b0b06d98c8814f6
aqiu0103 <2020003529@mails.cust.edu.cn>
2024-01-08 00:23:53 +0800

    通过tcp连接客户端和服务端并传输图片

--- javaWeb/ClientPoint.java
+++ javaWeb/ClientPoint.java
@@ -1,20 +1,40 @@
 package com.javaWeb;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Objects;
 
 public class ClientPoint {
     public static void main(String[] args) throws IOException {
         //连接服务器（ip，端口号），若连接上了就返回一个socket对象
         Socket socket=new Socket(InetAddress.getLocalHost(),9999);
         System.out.println("客户端发出请求，且已经和服务器连接上了，并得到一个客户端的socket");
-        //创建一个socket的输出流，将一个信息写入该输入流
+        //创建一个socket的输出流”管道“
         OutputStream outputStream = socket.getOutputStream();
-        outputStream.write("hello,server".getBytes());//getBytes()是将一个字符串拆开成一个个字节并存放在一个数组中
-        outputStream.close();
+        BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(outputStream);
+        //然后读取图片到内存中
+        BufferedInputStream bism = new BufferedInputStream(new FileInputStream("D:\\shit.jpg"));
+        byte[] buff = new byte[1024];
+        int readlen=0;
+        //一边读，一边传到socket的输出流里
+        while ((readlen= bism.read(buff))!=-1){
+            bufferedOutputStream.write(buff,0,readlen);
+        }
+        socket.shutdownOutput();//socket的输出流的结束标记，让服务端知道你已经输出结束了
+
+        //再确认一下服务端是否已经收到图片
+        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        if(Objects.equals(bufferedReader.readLine(), "服务端已收到图片")){
+            System.out.println("服务端确实已经收到图片了");
+        }
+        socket.shutdownInput();
+
+        //逐个关闭
+        bufferedReader.close();
+       // bufferedOutputStream.close();
+        bism.close();
         socket.close();
         System.out.println("客户端退出。。。");
     }
--- javaWeb/ServerPoint.java
+++ javaWeb/ServerPoint.java
@@ -1,7 +1,6 @@
 package com.javaWeb;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,12 +15,27 @@ public class ServerPoint {
         //接下来准备接收客户端发的消息
         //先创建一个属于服务器端的socket的输入流
         InputStream inputStream = socket.getInputStream();
+        BufferedInputStream bufferedInputStream=new BufferedInputStream(inputStream);
+        //创建一个输出流，输出到磁盘文件中
+        BufferedOutputStream bosm = new BufferedOutputStream(new FileOutputStream("D:\\shit2.jpg"));
         byte[] buff=new byte[1024];//创建一个缓存接收器
         int readlen=0;//每次输入流读一次字节流的长度
-        if((readlen= inputStream.read(buff))!=-1){
-            System.out.println(new String(buff,0,readlen));
+        //一边读取socket的输入流中的数据，一边把这些数据写到与本地磁盘相关的输出流管道里
+        while ((readlen= bufferedInputStream.read(buff))!=-1){
+            bosm.write(buff,0,readlen);
         }
-        inputStream.close();
+        socket.shutdownInput();//socket的输入流的结束标记，让客户端知道你已经输入结束了
+
+        //当服务端接收完图片后，再给客户端发个消息
+        BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        bufferedWriter.write("服务端已收到图片");
+        bufferedWriter.newLine();//表示已经写完了
+        bufferedWriter.flush();//调用这个flush（），刷新一下，才能写字符进socket的输出流里去
+
+        //逐个把资源关闭
+        bufferedWriter.close();
+        bosm.close();
+        bufferedInputStream.close();
         socket.close();
         serverSocket.close();
         System.out.println("服务端退出");
